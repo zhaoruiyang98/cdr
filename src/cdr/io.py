38e744ca28c9f63a984a9a885e4234d5175713d5
zhaoruiyang98 <zhaoruiyang19@mails.ucas.edu.cn>
2025-12-05 11:55:06 +0800

    add mpi_load_quijote_snapshot_table

--- src/cdr/io.py
+++ src/cdr/io.py
@@ -43,14 +43,163 @@ def mpi_load_fits_table(path, columns: list[str] | None = None, ext=None, comm: 
     return MPITable(data, comm=comm)
 
 
-def is_bool_mask(x):
+def load_quijote_snapshot_header(filename):
+    import h5py
+    import hdf5plugin  # noqa: F401
+
+    header: dict[str, Any] = {}
+    with h5py.File(filename, "r") as f:
+        f: Any
+        header["time"] = f["Header"].attrs["Time"]
+        header["redshift"] = f["Header"].attrs["Redshift"]
+        header["npart"] = (f["Header"].attrs["NumPart_ThisFile"]).astype(np.int64)
+        header["nall"] = (f["Header"].attrs["NumPart_Total"]).astype(np.int64)  # total number of particles
+        try:
+            header["nhigh"] = (f["Header"].attrs["NumPart_Total_HighWord"]).astype(np.int64)
+        except KeyError:
+            pass
+        else:
+            header["nall"] += header["nhigh"] << np.int64(32)
+        header["filenum"] = int(f["Header"].attrs["NumFilesPerSnapshot"])
+        header["massarr"] = f["Header"].attrs["MassTable"] * 1e10  # Masses of the particles in Msun/h
+        header["boxsize"] = f["Header"].attrs["BoxSize"] / 1e3  # Mpc/h
+        # check if it is a SWIFT snapshot
+        if "/Cosmology" in f.keys():
+            header["Omega0_m"] = f["Cosmology"].attrs["Omega_m"]
+            header["Omega0_Lambda"] = f["Cosmology"].attrs["Omega_lambda"]
+            header["h"] = f["Cosmology"].attrs["h"]
+        # check if it is a Gadget-4 snapshot
+        elif "/Parameters" in f.keys():
+            header["Omega0_m"] = f["Parameters"].attrs["Omega0"]
+            header["Omega0_Lambda"] = f["Parameters"].attrs["OmegaLambda"]
+            header["h"] = f["Parameters"].attrs["HubbleParam"]
+            # header['cooling']  = f['Parameters'].attrs[u'Flag_Cooling']
+        # if it is a traditional Gadget-1/2/3 snapshot
+        else:
+            header["Omega0_m"] = f["Header"].attrs["Omega0"]
+            header["Omega0_Lambda"] = f["Header"].attrs["OmegaLambda"]
+            header["h"] = f["Header"].attrs["HubbleParam"]
+            # header['cooling']  = f['Header'].attrs[u'Flag_Cooling']
+    return header
+
+
+def mpi_slice(n, comm: MPI.Intracomm | None = None):
+    comm = MPI.COMM_WORLD if comm is None else comm
+    rank = comm.rank
+    size = comm.size
+    q, r = divmod(n, size)
+    nlocal = q + (rank < r)
+    istart = q * rank + (rank if rank < r else r)
+    return slice(istart, istart + nlocal)
+
+
+def mpi_slice_size(n, comm: MPI.Intracomm | None = None):
+    sl = mpi_slice(n, comm)
+    return sl.stop - sl.start
+
+
+def mpi_read_quijote_field(filename, block: str, ptype: int, comm: MPI.Intracomm | None = None):
+    import h5py
+    import hdf5plugin  # noqa: F401
+
+    header = load_quijote_snapshot_header(filename)
+    mass = header["massarr"][ptype]
+    npart = header["npart"][ptype]
+    sl = mpi_slice(npart, comm)
+
+    prefix = f"PartType{ptype}/"
+    with h5py.File(filename, "r") as f:
+        f: Any
+        if block == "POS":
+            suffix = "Coordinates"
+        elif block == "MASS":
+            suffix = "Masses"
+        elif block == "ID":
+            suffix = "ParticleIDs"
+        elif block == "VEL":
+            suffix = "Velocities"
+        else:
+            raise NotImplementedError(f"block {block} not implemented!")
+
+        if f"{prefix}{suffix}" not in f.keys():
+            if mass != 0.0:
+                array = np.ones(sl.stop - sl.start, np.float32) * mass
+            else:
+                raise RuntimeError(f"Problem reading the block {block}")
+        else:
+            array = f[prefix + suffix][sl]
+    if block == "VEL":
+        array *= np.sqrt(header["time"])
+    if block == "POS" and array.dtype == np.float64:
+        array = array.astype(np.float32)
+    return array
+
+
+def mpi_load_quijote_snapshot_table(path, columns=["POS", "VEL"], ptypes=["cdm"], comm: MPI.Intracomm | None = None):
+    # adapted from pylians3: https://github.com/franciscovillaescusa/Pylians3
+    from pathlib import Path
+
+    path = Path(path)
+    ptypes = [ptype if isinstance(ptype, int) else {"cdm": 1, "neutrino": 2}[ptype.lower()] for ptype in ptypes]
+    comm = MPI.COMM_WORLD if comm is None else comm
+
+    # read header from the first file
+    if path.with_suffix(".hdf5").exists():
+        firstfile = path.with_suffix(".hdf5")
+    elif path.with_suffix(".0.hdf5").exists():
+        firstfile = path.with_suffix(".0.hdf5")
+    else:
+        raise FileNotFoundError(f"Could not find Quijote snapshot file at {path} with .hdf5 or .0.hdf5 suffix")
+    attrs = load_quijote_snapshot_header(firstfile)
+    attrs.pop("npart")
+    single_file = attrs["filenum"] == 1
+
+    ntotal = 0
+    for i in range(attrs["filenum"]):
+        if single_file:
+            filename = path.with_suffix(".hdf5")
+        else:
+            filename = path.with_suffix(f".{i}.hdf5")
+        ntotal += sum(mpi_slice_size(load_quijote_snapshot_header(filename)["npart"][ptype]) for ptype in ptypes)
+
+    data = {}
+    for column in columns:
+        if column == "POS":
+            dtype = np.dtype((np.float32, 3))
+        elif column == "VEL":
+            dtype = np.dtype((np.float32, 3))
+        elif column == "MASS":
+            dtype = np.float32
+        elif column == "ID":
+            dtype = mpi_read_quijote_field(firstfile, column, ptypes[0], comm=comm).dtype
+        else:
+            raise NotImplementedError(f"column {column} not found")
+        buffer = np.empty(ntotal, dtype=dtype)
+        offset = 0
+        for ptype in ptypes:
+            for i in range(attrs["filenum"]):
+                if single_file:
+                    filename = path.with_suffix(".hdf5")
+                else:
+                    filename = path.with_suffix(f".{i}.hdf5")
+                field = mpi_read_quijote_field(filename, column, ptype, comm=comm)
+                npart = field.shape[0]
+                buffer[offset : offset + npart] = field
+                offset += npart
+        data[column] = buffer
+
+    return MPITable(data, attrs=attrs, comm=comm)
+
+
+def _is_bool_mask(x):
     return isinstance(x, np.ndarray) and x.dtype == bool and x.ndim == 1
 
 
 class MPITable(MutableMapping):
-    def __init__(self, data: dict[str, Any], comm: MPI.Intracomm | None = None):
+    def __init__(self, data: dict[str, Any], attrs: dict[str, Any] | None = None, comm: MPI.Intracomm | None = None):
         self.comm = MPI.COMM_WORLD if comm is None else comm
 
+        self.attrs = {} if attrs is None else attrs
         self._data = {k: np.empty((0,)) if v is None else np.atleast_1d(v) for k, v in data.items()}
         colsize = max(v.shape[0] for v in self.values())
         if colsize > 1:
@@ -92,7 +241,7 @@ class MPITable(MutableMapping):
     def __getitem__(self, key: Any) -> Any:
         if isinstance(key, str):
             return self._data[key]
-        if is_bool_mask(key):
+        if _is_bool_mask(key):
             data = {k: v[key] for k, v in self.items()}
             return MPITable(data, comm=self.comm)
         if isinstance(key, list):
